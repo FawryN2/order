1f2149c66710c901389f67f5aa851e4d649cee16
Mohamedgamaal16 <mahooomar@yahoo.com>
2025-07-11 19:38:43 +0300

    first

---
+++ src/main/java/com/fawry_fridges/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.fawry_fridges.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fawry_fridges/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fawry_fridges.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
